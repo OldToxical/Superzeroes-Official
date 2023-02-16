9f3415175dca8b737e8a78a0a2715bc3d4c9974e
OldToxical <78077281+OldToxical@users.noreply.github.com>
2023-02-16 12:36:08 +0000

    Initial test commit

---
+++ Superzeroes/Intermediate/Build/Win64/UnrealEditor/Inc/Superzeroes/Superzeroes.init.gen.cpp
@@ -0,0 +1,29 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "UObject/GeneratedCppIncludes.h"
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+void EmptyLinkFunctionForGeneratedCodeSuperzeroes_init() {}
+	static FPackageRegistrationInfo Z_Registration_Info_UPackage__Script_Superzeroes;
+	FORCENOINLINE UPackage* Z_Construct_UPackage__Script_Superzeroes()
+	{
+		if (!Z_Registration_Info_UPackage__Script_Superzeroes.OuterSingleton)
+		{
+			static const UECodeGen_Private::FPackageParams PackageParams = {
+				"/Script/Superzeroes",
+				nullptr,
+				0,
+				PKG_CompiledIn | 0x00000000,
+				0xD3DF5ECA,
+				0xEFE59D46,
+				METADATA_PARAMS(nullptr, 0)
+			};
+			UECodeGen_Private::ConstructUPackage(Z_Registration_Info_UPackage__Script_Superzeroes.OuterSingleton, PackageParams);
+		}
+		return Z_Registration_Info_UPackage__Script_Superzeroes.OuterSingleton;
+	}
+	static FRegisterCompiledInInfo Z_CompiledInDeferPackage_UPackage__Script_Superzeroes(Z_Construct_UPackage__Script_Superzeroes, TEXT("/Script/Superzeroes"), Z_Registration_Info_UPackage__Script_Superzeroes, CONSTRUCT_RELOAD_VERSION_INFO(FPackageReloadVersionInfo, 0xD3DF5ECA, 0xEFE59D46));
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
---
+++ Superzeroes/Source/Superzeroes/SuperzeroesGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "SuperzeroesGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SUPERZEROES_API ASuperzeroesGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
