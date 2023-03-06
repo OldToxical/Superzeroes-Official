6782ee72397e3356c688384d69bbacf7260209f8
OldToxical <78077281+OldToxical@users.noreply.github.com>
2023-03-06 20:01:56 +0000

    Test Push with the new .gitignore

---
+++ Superzeroes/Source/Superzeroes/TestSphere.cpp
@@ -0,0 +1,28 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "TestSphere.h"
+
+// Sets default values
+ATestSphere::ATestSphere()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void ATestSphere::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ATestSphere::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+	SetActorLocation(FVector(GetActorLocation().X + 1.f, GetActorLocation().Y, GetActorLocation().Z));
+}
+
