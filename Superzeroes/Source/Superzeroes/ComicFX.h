fed0a744a111a58ae2aefe97f78ec7a1f8966532
OldToxical <78077281+OldToxical@users.noreply.github.com>
2023-03-27 18:20:11 +0300

    Added missing files for compilation

---
+++ Superzeroes/Source/Superzeroes/ComicFX.h
@@ -0,0 +1,44 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "PaperSpriteActor.h"
+#include "ComicFX.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SUPERZEROES_API AComicFX : public APaperSpriteActor
+{
+	GENERATED_BODY()
+
+public:
+	AComicFX();
+	~AComicFX();
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	UFUNCTION(BlueprintCallable)
+		void spriteChanger(int i);
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+		class UPaperSprite* zap;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+		class UPaperSprite* squeak; 
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+		class UPaperSprite* squawk;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+		class UPaperSprite* bang; 
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+		class UPaperSprite* oof;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+		class UPaperSprite* bam; 
+
+protected:
+		// Called when the game starts or when spawned
+		virtual void BeginPlay() override;
+		UPROPERTY(EditAnywhere, BlueprintReadOnly)
+			FVector location;
+};
