25dd670c034605cb9b1c2e94e2ea7ba976b5a8ef
OldToxical <2000717@uad.ac.uk>
2023-03-28 19:18:17 +0300

    Fix content folder and bugs fixed

--- Superzeroes/Source/Superzeroes/Enemy_Mouse.h
+++ Superzeroes/Source/Superzeroes/Enemy_Mouse.h
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #pragma once
@@ -122,4 +123,129 @@ protected:
 
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = FX)
 		TSubclassOf<class AComicFX> comicFX;
+=======
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Enemy.h"
+#include "BoomBoom.h"
+#include "ZipZap.h"
+#include "Kismet/GameplayStatics.h"
+#include "NiagaraFunctionLibrary.h"
+#include "NiagaraComponent.h"
+#include "Enemy_Mouse.generated.h"
+
+#define Q_LearningRate 0.9
+#define Q_DiscountFactor 0.2
+#define Q_EstimatedOptimalFutureValue 15
+#define MinimumDistanceToGetIntoCombatX 500
+#define MinimumDistanceToGetIntoCombatZ 50
+#define MinimumDistanceToDealDamage 100
+
+class ABoomBoom;
+class AZipZap;
+
+enum State4
+{
+	Idle,
+	Jumping,
+	WalkingLeft,
+	WalkingRight,
+	Attacking,
+	Dead
+};
+
+enum Action
+{
+	GoIdle,
+	Jump,
+	WalkLeft,
+	WalkRight
+};
+
+UCLASS()
+class SUPERZEROES_API AEnemy_Mouse : public AEnemy
+{
+	GENERATED_BODY()
+
+private:
+	AEnemy_Mouse();
+
+	virtual void AI() override;
+	virtual void BeginPlay() override;
+	virtual void Tick(float DeltaTime) override;
+	virtual void TakeEnemyDamage(float damage_);
+
+	// AI Functions
+	void GetState();
+	void ChooseAction();
+	void CalculateReward();
+	void UpdateQ(float reward);
+	void ExecuteAction();
+	void UpdateState();
+
+	// Regular Functions
+	void WalkLeft();
+	void WalkRight();
+	void Attack();
+	void GoToPlayer();
+	void DealDamage();
+
+	float chooseActionTimeoutTimer;
+	float stateUpdateTimer;
+	float speed;
+	float newX;
+	State4 currentState;
+	Action currentAction;
+	TArray<TArray<float, TFixedAllocator<4>>, TFixedAllocator<4>> AI_Q;
+
+	UPROPERTY(EditAnywhere)
+	    bool inCombat;
+
+	bool hitAvailable;
+
+	// Animations
+	UPROPERTY(EditAnywhere)
+	    UPaperFlipbook* idle;
+
+	UPROPERTY(EditAnywhere)
+		UPaperFlipbook* walk;
+
+	UPROPERTY(EditAnywhere)
+		UPaperFlipbook* attack;
+
+	UPROPERTY(EditAnywhere)
+		UPaperFlipbook* dead;
+
+	UPROPERTY(EditAnywhere)
+		UPaperFlipbook* jumpAnim;
+
+	UPROPERTY(EditAnywhere)
+		UPaperFlipbook* hurtAnim;
+
+	// Particles
+	UPROPERTY(EditAnywhere)
+		UParticleSystem* boomBoomImpact;
+
+protected:
+	UFUNCTION(BlueprintCallable)
+	    void EndAttack();
+
+	// Called when landed
+	virtual void Landed(const FHitResult& Hit) override;
+
+	UPROPERTY(BlueprintReadWrite)
+	    ABoomBoom* boomBoom;
+
+	UPROPERTY(BlueprintReadWrite)
+	    AZipZap* zipZap;
+
+	UPROPERTY(VisibleAnywhere)
+		AActor* playerToAttack;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = FX)
+		TSubclassOf<class AComicFX> comicFX;
+>>>>>>> Stashed changes
 };
